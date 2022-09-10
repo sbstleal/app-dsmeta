ab66780efa206bd0b0edc4839c2c0fc0522202eb
Sebastião Leal <sebastiaodeoleal@gmail.com>
2022-09-10 16:37:45 -0300

    Adding Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -3,10 +3,14 @@ package com.devsuperior.dsmeta.controller;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -17,7 +21,10 @@ public class SaleController {
     private SaleService service;
 
     @GetMapping
-    public List<Sale> findSale() {
-        return service.findSales();
+    public Page<Sale> findSale(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return service.findSales(minDate, maxDate, pageable);
     }
 }
