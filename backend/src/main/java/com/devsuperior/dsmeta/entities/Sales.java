f9070219ddcb2c3bfd6db135edfc19a524e6cfb8
Sebastião Leal <sebastiaodeoleal@gmail.com>
2022-09-10 00:05:56 -0300

    Adding H2-Databae

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sales.java
@@ -0,0 +1,70 @@
+package com.devsuperior.dsmeta.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sales {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private String visited;
+    private String deals;
+    private Double amount;
+    private LocalDate date;
+
+    public Sales() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public String getVisited() {
+        return visited;
+    }
+
+    public void setVisited(String visited) {
+        this.visited = visited;
+    }
+
+    public String getDeals() {
+        return deals;
+    }
+
+    public void setDeals(String deals) {
+        this.deals = deals;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
