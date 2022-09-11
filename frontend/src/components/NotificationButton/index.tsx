fdabeb7d22ccf7170bbe3c011a89eb29ee081591
Sebastião Leal <sebastiaodeoleal@gmail.com>
2022-09-10 21:10:35 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
 import './styles.css';
 
-import icon from '../../assets/img/notification-icon.svg';
+import axios from 'axios';
 
-function NotificationButton() {
+import icon from '../../assets/img/notification-icon.svg';
+import { Props } from '../../models/props';
+import { BASE_URL } from '../../utils/request';
+
+
+function handleClick(saleId: number) {
+    axios.get(`${BASE_URL}/sales/${saleId}/notification`)
+    .then(response => {
+        console.log("Success");
+    })
+}
+
+function NotificationButton({saleId}: Props) {
     return (
-            <div className="dsmeta-red-btn">
+            <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
                 <img src={icon} alt="Ícone de Notificação" />
             </div>
     )
