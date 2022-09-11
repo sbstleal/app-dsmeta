13eeb48cac170e1c188091b8105d28683ffd42cf
Sebastião Leal <sebastiaodeoleal@gmail.com>
2022-09-10 21:29:22 -0300

    Adding Toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+window.React = React
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
